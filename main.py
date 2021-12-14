304fe9524a2facd13ed6c4eb784e71584b99a715
BuildTools <unconfigured@null.spigotmc.org>
2021-12-14 21:46:54 +0200

    Refactor code, new features

--- main.py
+++ main.py
@@ -3,12 +3,27 @@ import re
 import requests
 from bs4 import BeautifulSoup as BS
 
-product = 'AMD Ryzen 5 Cezanne'
+print('---------')
+print('Enter model for search:')
+product = input()
+print('Enter sort type:')
+print('(True - sorts the list in the descending order')
+print('(False - sorts the list in the ascending order')
 
-r = requests.get("https://ek.ua/ek-list.php?search_={0}".format(product))
-html = BS(r.content, "html.parser")
 
-price_list = []
+# Check true or false
+def sort_type_check():
+    while True:
+        user_answer = input().lower().strip()
+        if user_answer == "true":
+            return True
+        elif user_answer == "false":
+            return False
+        else:
+            print("Error: Answer must be True or False")
+
+
+sort_type = sort_type_check()
 
 
 # Get price from text
@@ -18,34 +33,48 @@ def get_price(price_text):
     return int(re.search(r"\d+", price_text_no_spaces).group(0))
 
 
-# Print list with ', '
-def print_list(list_print):
-    for i in list_print:
+# Print list of prices with ', ' and sorted
+def print_list_prices(html_prices):
+    price_list = []
+    for el in html_prices.select(".tr-odd"):
+        price = el.select(".where-buy-price")
+        price_list.append(get_price(price[0].text))
+    for i in price_list:
         print(i, end=', ')
+    price_list.sort(reverse=sort_type)
 
 
-def search_link(html_search_link):
-    for elem in html_search_link.select(".model-short-info"):
+# Search link product on ek
+def search_link_product(link_product):
+    for elem in link_product.select(".model-short-info"):
         href = elem.select('a')
         return href[0].get("href")
 
 
-def search_link_button(html_search_link_button):
-    for elem in html_search_link_button.select(".main-part-content > .desc-menu"):
+# Search link for prices on ek
+def search_link_for_prices(link_for_prices):
+    for elem in link_for_prices.select(".main-part-content > .desc-menu"):
         href = elem.select('a')
         return href[0].get("link")
 
 
-print(search_link(html))
-r2 = requests.get("https://ek.ua{0}".format(search_link(html)))
-html2 = BS(r2.content, "html.parser")
-print(search_link_button(html2))
-r3 = requests.get("https://ek.ua{0}".format(search_link_button(html2)))
-html3 = BS(r3.content, "html.parser")
+# Search link
+r = requests.get("https://ek.ua/ek-list.php?search_={0}".format(product))
+html = BS(r.content, "html.parser")
+print('Search link')
+print(r.url)
 
-for el in html3.select(".tr-odd"):
-    price = el.select(".where-buy-price")
-    price_list.append(get_price(price[0].text))
+# Product link
+r = requests.get("https://ek.ua{0}".format(search_link_product(html)))
+html = BS(r.content, "html.parser")
+print('Product link')
+print(r.url)
 
-price_list.sort()
-print_list(price_list)
+# Prices link
+r = requests.get("https://ek.ua{0}".format(search_link_for_prices(html)))
+html = BS(r.content, "html.parser")
+print('Prices link')
+print(r.url)
+
+# Print all sorted prices
+print_list_prices(html)
